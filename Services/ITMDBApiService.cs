b7c7edba0011d0a289873cd715ba46baa81eedaa
Andrey Malykhin <andrei.malihin@ati.utm.md>
2021-12-02 21:53:44 +0200

    Move TMDB api key to appsettings.json

--- Services/ITMDBApiService.cs
+++ Services/ITMDBApiService.cs
@@ -9,20 +9,20 @@ namespace TMDBapp.Services
 {
     public interface ITMDBApiService
     {
-        [Get("/movie/popular?api_key=dad8a59d86a2793dda93aa485f7339c1")]
-        Task<PaginatedResponse<Movie>> GetMostPopular([Query] int? page);
+        [Get("/movie/popular?api_key={apiKey}")]
+        Task<PaginatedResponse<Movie>> GetMostPopular(string apiKey, [Query] int? page);
 
-        [Get("/movie/top_rated?api_key=dad8a59d86a2793dda93aa485f7339c1")]
-        Task<PaginatedResponse<Movie>> GetTopRated([Query] int? page);
+        [Get("/movie/top_rated?api_key={apiKey}")]
+        Task<PaginatedResponse<Movie>> GetTopRated(string apiKey, [Query] int? page);
 
-        [Get("/genre/movie/list?api_key=dad8a59d86a2793dda93aa485f7339c1")]
-        Task<GenreResponse> GetGenres();
+        [Get("/genre/movie/list?api_key={apiKey}")]
+        Task<GenreResponse> GetGenres(string apiKey);
         
-        [Get("/discover/movie?api_key=dad8a59d86a2793dda93aa485f7339c1&with_genres={genreId}&page={page}")]
-        Task<PaginatedResponse<Movie>> GetByGenre(string sortDirection, int genreId, int page);
+        [Get("/discover/movie?api_key={apiKey}&with_genres={genreId}&page={page}")]
+        Task<PaginatedResponse<Movie>> GetByGenre(string apiKey, string sortDirection, int genreId, int page);
 
-        [Get("/movie/{id}?api_key=dad8a59d86a2793dda93aa485f7339c1")]
-        Task<MovieDetails> GetDetails(int id);
+        [Get("/movie/{id}?api_key={apiKey}")]
+        Task<MovieDetails> GetDetails(string apiKey, int id);
     }
 
     public class GenreResponse
--- Services/TMDBMovieService.cs
+++ Services/TMDBMovieService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -12,29 +13,33 @@ namespace TMDBapp.Services
     {
         private readonly ITMDBApiService api;
         private readonly IFavouritesRepository favRepo;
+        private readonly string apiKey;
 
-        public TMDBMovieService(ITMDBApiService api, IFavouritesRepository favRepo) 
-            => (this.api, this.favRepo) = (api, favRepo);
+        public TMDBMovieService(ITMDBApiService api, IFavouritesRepository favRepo, IConfiguration config)
+        {
+            this.apiKey = config["ApiKey"];
+            (this.api, this.favRepo) = (api, favRepo);
+        }
 
         public void AddFavourite(int movieId, string userId) 
             => favRepo.AddFavourite(movieId, userId);
 
         public PaginatedResponse<Movie> GetByGenre(string userId, int genreId, int page, string sortDirection = "desc")
         {
-            var response = api.GetByGenre(sortDirection, genreId, page).Result;
+            var response = api.GetByGenre(apiKey, sortDirection, genreId, page).Result;
             response.SortDirection = sortDirection;
             return PopulateFavourites(userId, response);
         }
 
         public MovieDetails GetDetails(int movieId)
-            => api.GetDetails(movieId).Result;
+            => api.GetDetails(apiKey, movieId).Result;
 
         public IEnumerable<Genre> GetGenres()
-            => api.GetGenres().Result.Genres;
+            => api.GetGenres(apiKey).Result.Genres;
 
         public PaginatedResponse<Movie> GetMostPopular(string userId, int? page)
         {
-            var response = api.GetMostPopular(page).Result;
+            var response = api.GetMostPopular(apiKey, page).Result;
             return PopulateFavourites(userId, response);
         }
 
@@ -45,14 +50,14 @@ namespace TMDBapp.Services
             {
                 if (totalPages is null)
                 {
-                    response = api.GetTopRated(page).Result;
+                    response = api.GetTopRated(apiKey, page).Result;
                     totalPages = response.TotalPages;
                 }
-                response = api.GetTopRated(totalPages + 1 - page).Result;
+                response = api.GetTopRated(apiKey, totalPages + 1 - page).Result;
                 response.Page = page;
             } else
             {
-                response = api.GetTopRated(page).Result;
+                response = api.GetTopRated(apiKey, page).Result;
             }
             response.SortDirection = sortDirection;
             return PopulateFavourites(userId, response);
